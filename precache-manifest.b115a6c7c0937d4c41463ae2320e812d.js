a081aa9d2ff77f75f0148863c1fc74bbcf7036ed
haelmorn <haelmorn@gmail.com>
2019-08-06 22:45:54 +0200

    Updates

---
+++ precache-manifest.b115a6c7c0937d4c41463ae2320e812d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeb4bd2faed2ed813e04c5dff461f0d7",
+    "url": "/quote-generator/index.html"
+  },
+  {
+    "revision": "8ffc2c0a53228b99f648",
+    "url": "/quote-generator/static/css/main.d55d9aa8.chunk.css"
+  },
+  {
+    "revision": "f97963dd59e585a2a2ac",
+    "url": "/quote-generator/static/js/2.42c80c5a.chunk.js"
+  },
+  {
+    "revision": "8ffc2c0a53228b99f648",
+    "url": "/quote-generator/static/js/main.4d9b54dd.chunk.js"
+  },
+  {
+    "revision": "b42996cbb3818e11c29d",
+    "url": "/quote-generator/static/js/runtime~main.4226e67e.js"
+  }
+]);
